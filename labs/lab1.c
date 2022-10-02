1bfe8419f737b456b7d14b0ef1ed8d55de5e2c58
glebins <43817491+Glebins@users.noreply.github.com>
2022-10-02 22:59:43 +0300

    Update lab1.c

--- labs/lab1.c
+++ labs/lab1.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+long long get_number_without_numbers(long long x, int* number_zeros);
+
+int main()
+{
+  long long a;
+  int number = 0;
+  printf("Enter the number: ");
+  scanf("ld", &a);
+  
+  long long w = get_number_without_zeros(a, &number);
+}
